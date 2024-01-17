8aa97e32fa888dce31f32d49a444f274d255a8d6
jrazap <elazapm@gmail.com>
2024-01-17 11:49:34 +0200

    Change date

--- src/components/copyright.js
+++ src/components/copyright.js
@@ -2,7 +2,7 @@ const Copyright = () => {
   return (
     <div className="credit">
       <p>
-        <span>© 2021 | Proudly created with</span>{" "}
+        <span>&copy; {new Date().getFullYear()} | Proudly created with</span>{" "}
         <a href="https://jrazap.com/">Mohamed Elazap</a>
       </p>
     </div>
